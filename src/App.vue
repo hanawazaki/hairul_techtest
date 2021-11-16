a607ae6fb6a688381e368122bbcb1c2ae7bbebdd
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2021-11-16 07:59:55 +0700

    init push

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from "./components/Navbar.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+  },
+};
 </script>
 
 <style lang="scss">
@@ -19,8 +17,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  // text-align: center;
+  // color: #2c3e50;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <header>
+    <div class="logo">
+      <img src="../assets/logo.png" alt="logo" />
+    </div>
+    <div>
+      <ul class="menu">
+        <li>
+          <a href="#" class="link active">Home</a>
+        </li>
+        <li>
+          <a href="#" class="link">Discover</a>
+        </li>
+        <li>
+          <a href="#" class="register">Register</a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="../assets/search.png" alt="search" style="width: 20px" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass" scoped>
+header
+  display: flex
+  justify-content: space-between
+  padding: 0 16px
+  background-color: #F7F9F9
+  .logo
+    padding: 10px 0px
+    img
+      width: 48px
+
+  .menu
+    display: flex
+    flex-direction: row
+    li
+      list-style: none
+      margin: 0 20px
+      padding: 10px 0px
+      a
+        text-decoration: none
+        color: #000
+        &.active
+          background-color: #E0E2E2
+          border-radius: 30px
+          padding: 10px 40px
+    .register
+      margin-left: 30px
+</style>
